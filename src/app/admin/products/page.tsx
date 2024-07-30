c4da578c73e4af7a1ddec362beec2c649b781cf4
Moha <hassanhuriye05@gmail.com>
2024-07-30 20:27:13 +0300

    product page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,18 +1,39 @@
 import { Button } from "@/components/ui/button"
 import { PageHeader } from "../_components/PageHeader"
-import { Link } from "lucide-react"
+import Link from "next/link"
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
 
 export default function AdminProductsPage() {
     return (
         <>
         <div className="flex justify-between items-center gap-4">
         <PageHeader>Products</PageHeader>
-        <Button>
+        <Button asChild>
             <Link href="/admin/produts/new">Add Product</Link>
         </Button>
         </div>
-          
+          <ProductTable />
         </>
     )
     
+}
+
+
+function ProductTable() {
+    return <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead className="w-0">
+                    <span className="sr-only">Availible For Purchase</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Orders</TableHead>
+                <TableHead className="w-0">
+                    <span className="sr-only">Actions</span>
+                </TableHead>
+            </TableRow>
+        </TableHeader>
+    </Table>
 }
