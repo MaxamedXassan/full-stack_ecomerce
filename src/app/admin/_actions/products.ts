4a88233aa6444241e041996782ab13c74307f16e
Moha <hassanhuriye05@gmail.com>
2024-08-21 18:47:15 +0300

    productSchema

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,8 +1,11 @@
 "use server"
 
-import db from "@/db/db"
+
 import { object, z } from "zod"
 import fs from "fs/promises"
+import { redirect } from "next/navigation"
+import db from "@/db/db"
+
 
 
 const fileSchema = z.instanceof(File, {message: "Required"})
@@ -13,30 +16,35 @@ const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.start
     description: z.string().min(1),
     priceInCents: z.coerce.number().int().min(1),
     file: fileSchema.refine(file => file.size > 0, "Required"),
-    image: fileSchema.refine(file => file.size > 0, "Required"),
+    image: imageSchema.refine(file => file.size > 0, "Required"),
 })
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
    if (result.success === false) {
     return result.error.formErrors.fieldErrors
    }
 
    const data = result.data
-
+   
+   
     await fs.mkdir("products", {recursive: true})
-    const filePath = `product/${crypto.randomUUID()}-${data.file.name}`
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
     await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
 
     await fs.mkdir("public/products", {recursive: true})
-    const imagePath = `/product/${crypto.randomUUID()}-${data.image.name}`
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
     await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
 
-   db.product.create({data: {
+   await db.product.create({data: {
+   isAvailableForPurchase: false,
     name: data.name,
     description: data.description,
     priceInCents: data.priceInCents,
     filePath,
     imagePath
-   }})
+   }
+})
+
+   redirect('/admin/products')
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button"
 import { PageHeader } from "../_components/PageHeader"
 import Link from "next/link"
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react"
+import db from "@/db/db"
+import { formatCurency, formatNumber } from "@/lib/formatters"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 
 
 export default function AdminProductsPage() {
@@ -20,7 +24,20 @@ export default function AdminProductsPage() {
 } 
 
 
-function ProductTable() {
+ async function ProductTable() {
+    const products = await db.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            priceInCents: true,
+            isAvailableForPurchase: true,
+            _count: {select: {orders: true}},
+        },
+        orderBy: {name: "asc"}
+    })
+
+    if (products.length === 0) return<p>No products found</p>
+
     return <Table>
         <TableHeader>
             <TableRow>
@@ -35,5 +52,46 @@ function ProductTable() {
                 </TableHead>
             </TableRow>
         </TableHeader>
+       <TableBody>
+        {products.map(product => (
+            <TableRow key={product.id}>
+                <TableCell>
+                        {product.isAvailableForPurchase ? (
+                         <>
+                         <span className="sr-only">Available</span>
+                        <CheckCircle2 /> 
+                        </> ) : ( 
+                            <>
+                            <XCircle />
+                            <span className="sr-only">Unavailable</span>
+                            </> )}
+                </TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{formatCurency(product.priceInCents / 100)}</TableCell>
+                <TableCell>{formatNumber(product._count.orders / 100)}</TableCell>
+                <TableCell>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                    <MoreVertical />
+                    <span className="sr-only">Actions</span>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem asChild>
+                            <a download href={`/admin/products/${product.id}/download`}>
+                            Download
+                            </a>
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem asChild>
+                           <Link href={`/admin/products/${product.id}/edit`}>
+                           Edit
+                           </Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
+                </TableCell>
+            </TableRow>
+        ))}
+       </TableBody>
     </Table>
 }
